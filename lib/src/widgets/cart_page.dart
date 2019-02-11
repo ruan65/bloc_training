847eb3da544e050f5c65baa6905e67039fa755aa
ruan65 <ruan65@gmail.com>
2019-02-11 17:49:31 +0300

    cart page is working

--- lib/src/widgets/cart_page.dart
+++ lib/src/widgets/cart_page.dart
@@ -1,16 +1,59 @@
+import 'package:bloc_example/src/bloc/cart_provider.dart';
+import 'package:bloc_example/src/model/cart.dart';
+import 'package:bloc_example/src/model/cart_item.dart';
+import 'package:bloc_example/src/utils/is_dark.dart';
 import 'package:flutter/material.dart';
 
 class CartPage extends StatelessWidget {
-
   static const routeName = "/cart";
+
+  CartPage();
 
   @override
   Widget build(BuildContext context) {
+    final cart = CartProvider.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your cart'),
+      appBar: AppBar(title: Text('Your cart')),
+      body: StreamBuilder<List<CartItem>>(
+        stream: cart.items,
+        builder: (context, snapshot) {
+          if (snapshot.data == null || snapshot.data.isEmpty) {
+            return Center(
+              child: Text('Empty', style: Theme.of(context).textTheme.display1),
+            );
+          }
+          return ListView(
+              children:
+                  snapshot.data.map((item) => ItemTile(item: item)).toList());
+        },
       ),
-      body: null,
+    );
+  }
+}
+
+class ItemTile extends StatelessWidget {
+  final CartItem item;
+
+  const ItemTile({Key key, this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+        color: isDark(item.product.color) ? Colors.white : Colors.black);
+
+    return Container(
+      color: item.product.color,
+      child: ListTile(
+        title: Text(item.product.name, style: textStyle),
+        trailing: CircleAvatar(
+          backgroundColor: const Color(0x33ffffff),
+          child: Text(
+            item.count.toString(),
+            style: textStyle,
+          ),
+        ),
+      ),
     );
   }
 }
